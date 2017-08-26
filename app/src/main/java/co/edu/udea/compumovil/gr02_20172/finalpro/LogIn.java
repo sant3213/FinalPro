3cff4b6c99c8bb7e55233e7f26a4b92c9bd55921
Carlos Jiménez <cjimenez@melollevo.com>
2017-08-25 20:37:56 -0500

    makes the last features

--- app/src/main/java/co/edu/udea/compumovil/gr02_20172/finalpro/LogIn.java
+++ app/src/main/java/co/edu/udea/compumovil/gr02_20172/finalpro/LogIn.java
@@ -3,6 +3,10 @@ package co.edu.udea.compumovil.gr02_20172.finalpro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,6 +24,31 @@ public class LogIn extends AppCompatActivity {
         startActivity(reg);
       }
     });
+    Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+    setSupportActionBar(myToolbar);
+  }
 
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.update_profile:
+        Intent registroIntent = new Intent(getApplicationContext(), Registro.class);
+        startActivity(registroIntent);
+        return true;
+      case R.id.get_out:
+        Intent exitIntent = new Intent(Intent.ACTION_MAIN);
+        exitIntent.addCategory(Intent.CATEGORY_HOME);
+        exitIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(exitIntent);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu, menu);
+    return true;
   }
 }
--- app/src/main/java/co/edu/udea/compumovil/gr02_20172/finalpro/Registro.java
+++ app/src/main/java/co/edu/udea/compumovil/gr02_20172/finalpro/Registro.java
@@ -1,71 +1,47 @@
 package co.edu.udea.compumovil.gr02_20172.finalpro;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.icu.util.Calendar;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity implements View.OnClickListener {
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
+
+import gun0912.tedbottompicker.TedBottomPicker;
+
+public class Registro extends AppCompatActivity {
   Button btn_registrar;
-
-  //variables fecha
   Button btn_fecha;
   EditText txt_fecha;
-  private int dia, mes, año;
-
-  //variables galeria
-  private static final int SELECTED_PICTURE = 1;
-  //variables camara
-  private final int REQUEST_IMAGE_CAPTURE = 1;
-
-  ImageView imageView;
-  private Button btn_toma_foto;
-  private Button btn_galeria;
+  AutoCompleteTextView  autocomplete;
+  String[] cities = { "Bogotá", "Medellín","Cali", "Armenia", "Pasto"};
+  ImageView imageHolder;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_registro);
-    //configuración camara
+    initViewElements();
+    setAutocomplete();
 
-    //  magicalCamera=new MagicalCamera(this,RESIZE_PHOTO_PIXELS_PERCENTAGE);
-
-    imageView = (ImageView) findViewById(R.id.imgfoto);
-    btn_galeria = (Button) findViewById(R.id.btngaleria);
-    btn_toma_foto = (Button) findViewById(R.id.btntomarfoto);
-
-    btn_toma_foto.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        foto();
-      }
-    });
-
-    btn_galeria.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent gall=new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(gall, SELECTED_PICTURE);
-      }
-    });
-
-    //Configuración botón fecha
-    txt_fecha = (EditText) findViewById(R.id.txtfecha);
-    btn_fecha = (Button) findViewById(R.id.btnfecha);
-    btn_fecha.setOnClickListener(this);
-
-    //configuración botón registrar
-    btn_registrar =(Button)findViewById(R.id.btnregistrar);
     btn_registrar.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -75,43 +51,90 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
     });
   }
 
-  private void foto() {
-    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-      startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-    }
-  }
-  //para tomar fotos
-  @Override
-  public void onActivityResult(int requestCode, int resultCode,Intent data){
-    if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-      Bundle extras=data.getExtras();
-      Bitmap imageBitmap=(Bitmap)extras.get("data");
-      imageView.setImageBitmap(imageBitmap);
-    }
-    if(requestCode == SELECTED_PICTURE && resultCode == RESULT_OK){
-      Uri path=data.getData();
-      imageView.setImageURI(path);
-
-    }
-  }
-
   @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.N)
+  public void setBrithDate(View v) {
+    Calendar callendar = Calendar.getInstance();
+    int dia = callendar.get(Calendar.DAY_OF_MONTH);
+    int mes = callendar.get(Calendar.MONTH);
+    int año = callendar.get(Calendar.YEAR);
+    DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+      @Override
+      public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        txt_fecha.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+      }
+    }, año, mes, dia);
+    datePickerDialog.show();
+  }
+
+  private void setAutocomplete() {
+    autocomplete = (AutoCompleteTextView) findViewById(R.id.city);
+    ArrayAdapter<String> adapter = new ArrayAdapter (this, android.R.layout.select_dialog_item, cities);
+    autocomplete.setThreshold(2);
+    autocomplete.setAdapter(adapter);
+  }
+
+  private void initViewElements() {
+    txt_fecha = (EditText) findViewById(R.id.txtfecha);
+    btn_fecha = (Button) findViewById(R.id.btnfecha);
+    btn_registrar =(Button)findViewById(R.id.btnregistrar);
+    imageHolder = (ImageView) findViewById(R.id.profileImageHolder);
+  }
+
+  public void setImageUser(View v) {
+    PermissionListener permissionlistener = new PermissionListener() {
+      @Override
+      public void onPermissionGranted() {
+//        Toast.makeText(Registro.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        callSetImageUser();
+      }
+
+      @Override
+      public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+        Toast.makeText(Registro.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+      }
+    };
+    TedPermission.with(this)
+        .setPermissionListener(permissionlistener)
+        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+        .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        .check();
+  }
+  private void callSetImageUser() {
+    TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(this)
+        .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
+          @Override
+          public void onImageSelected(Uri uri) {
+            setImageHolder();
+            imageHolder.setImageURI(uri);
+          }
+        }).create();
+    tedBottomPicker.show(getSupportFragmentManager());
+  }
+
   @Override
-  public void onClick(View v) {
-    if (v == btn_fecha) {
-      final Calendar c = Calendar.getInstance();
-      dia = c.get(Calendar.DAY_OF_MONTH);
-      mes = c.get(Calendar.MONTH);
-      año = c.get(Calendar.YEAR);
-      DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-          txt_fecha.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-        }
-      }, dia, mes, año);
-      datePickerDialog.show();
+  protected void onSaveInstanceState(Bundle savedInstance) {
+    super.onSaveInstanceState(savedInstance);
+    BitmapDrawable drawable = (BitmapDrawable) imageHolder.getDrawable();
+    if(drawable != null) {
+      Bitmap bitmap = drawable.getBitmap();
+      savedInstance.putParcelable("image", bitmap);
     }
   }
+
+  @Override
+  public void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    if(savedInstanceState.containsKey("image")) {
+      Bitmap bitmap = savedInstanceState.getParcelable("image");
+      setImageHolder();
+      imageHolder.setImageBitmap(bitmap);
+    }
+  }
+
+  private void setImageHolder() {
+    ViewGroup.LayoutParams params = imageHolder.getLayoutParams();
+    params.height = 500;
+    imageHolder.setLayoutParams(params);
+  }
+
 }
